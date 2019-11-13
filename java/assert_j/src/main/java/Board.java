259f20d190eb206de832b5447a094b9465865ece
Krystian Adamski <krystian.adamski@allegro.pl>
2019-11-13 21:58:18 +0100

    1

---
+++ java/assert_j/src/main/java/Board.java
@@ -0,0 +1,58 @@
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Board {
+    Set<Point> points = new HashSet<>();
+
+    public static Board empty() {
+        return new Board();
+    }
+
+    public Board iterate() {
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return Objects.equals(points, board.points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
+    }
+}
+
+class Point {
+    private final int x;
+    private final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
+
+
+
+
+
--- java/assert_j/src/test/java/ThingTest.java
+++ java/assert_j/src/test/java/ThingTest.java
@@ -3,11 +3,15 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThingTest {
-
     @Test
-    void it_should_call_for_action() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertThat(value).isEqualTo("Food");
+    public void shouldEvaluateEmptyBoard() {
+        //given
+        final var empty = Board.empty();
+
+        //when
+        Board result = empty.iterate();
+
+        //then
+        assertThat(result).isEqualTo(Board.empty());
     }
 }
